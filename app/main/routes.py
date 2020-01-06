a21a4026faa8c38b2a9090b495404332535e0873
Brent V. Bingham <bvbgrad@gmail.com>
2020-01-05 20:08:39 -0600

    Obtain disk volume information

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,6 +5,9 @@ from flask_login import current_user, login_required
 from app import db
 from app.main import bp
 
+import subprocess
+import os
+
 
 @bp.before_app_request
 def before_request():
@@ -30,3 +33,21 @@ def index():
     ]
 
     return render_template('main/index.html', admin_type=current_user.admin_type,  posts=posts)
+
+
+@bp.route('/vol')
+def vol():
+    current_app.logger.info('vol')
+    volCmd = "vol e:"
+    volData = os.system(volCmd)
+    if (volData == 0):
+        stream = os.popen(volCmd)
+        volName = stream.readline()
+        volSerialNumber = stream.readline()
+        print("volume: ", volName)
+        print("Serial: ", volSerialNumber)
+        return render_template('main/vol.html', volName=volName, volSerialNumber=volSerialNumber)
+    else:
+        no_data = "No disk data"
+        print(no_data)
+        return render_template('main/vol.html', volName=no_data)
