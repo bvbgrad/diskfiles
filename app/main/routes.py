2b3e15305ca9b5520a99c83560e91f5528028245
Brent V. Bingham <bvbgrad@gmail.com>
2020-01-24 06:15:24 -0600

    Obtain and post a list of all the files on a disk

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,10 +1,11 @@
 """
     Routes for Main application
 """
-from datetime import datetime
 import os
+from datetime import datetime
+from pathlib import Path
 
-from flask import render_template, current_app
+from flask import current_app, render_template
 from flask_login import current_user, login_required
 
 from app import db
@@ -39,15 +40,12 @@ def vol():
     """
         stub route to test disk vol command
     """
-    volume_info = __vol_info__('e:')
+    volume_letter = 'E:'
+    volume_info = __vol_info__(volume_letter)
     if len(volume_info) == 0:
         return render_template('main/vol.html', volName="No disk data")
 
-    disk_files = []
-    disk_files.append(("serial number", "volume name", "file name"))
-    disk_files.append((volume_info["serial_number"], volume_info["name"], "file 1"))
-    disk_files.append((volume_info["serial_number"], volume_info["name"], "file 2"))
-
+    disk_files = __create_disk_list__(volume_letter)
     return render_template(
         'main/vol.html', disk_files=disk_files,
         volName=volume_info["name"],
@@ -72,3 +70,26 @@ def __vol_info__(volume_letter):
             ", Name: " + volume_info["name"])
 
     return volume_info
+
+
+def __create_disk_list__(current_dir):
+    current_app.logger.info("Enter: main/__create_disk_list__ from volume: " + current_dir)
+
+    disk_files = []
+    disk_files.append(("Path", "Size", "Created"))
+
+    current_path = Path(current_dir)
+    for path in sorted(current_path.rglob('*')):
+        if path.is_file():
+            filepath = path.drive / path.parent / path.name
+            filename = str(filepath)[2:] # get rid of drive letter
+            created = datetime.fromtimestamp(path.stat().st_ctime)
+            disk_files.append((filename, path.stat().st_size,\
+                            f"{created:%Y%m%d-%H:%M:%S}"))
+        else:
+            # print(path.name)
+            pass
+
+    current_app.logger.info(
+        f"Exit: main/__create_disk_list__ found {len(disk_files) - 1} files")
+    return disk_files
